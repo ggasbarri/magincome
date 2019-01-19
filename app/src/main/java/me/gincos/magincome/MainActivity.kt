feb75a33d7c63b121eeb4ea3b6ece6b4331902c8
Gianfranco Gasbarri <gifrgas@gmail.com>
2019-01-19 19:34:26 -0400

    feat: Firebase Auth logic added

--- app/src/main/java/me/gincos/magincome/MainActivity.kt
+++ app/src/main/java/me/gincos/magincome/MainActivity.kt
@@ -1,10 +1,13 @@
 package me.gincos.magincome
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import me.gincos.magincome.R
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,11 +19,47 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         auth = FirebaseAuth.getInstance()
-    }
-
-    override fun onStart() {
-        super.onStart()
 
         user = auth.currentUser
+
+        if (user == null) {
+            // Choose authentication providers
+            val providers = arrayListOf(
+                AuthUI.IdpConfig.EmailBuilder().build(),
+                AuthUI.IdpConfig.GoogleBuilder().build()
+            )
+
+            // Create and launch sign-in intent
+            startActivityForResult(
+                AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    .setTheme(R.style.AppTheme)
+                    .build(),
+                RC_SIGN_IN
+            )
+        }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                user = FirebaseAuth.getInstance().currentUser
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
+    }
+
 }
+
+const val RC_SIGN_IN = 1;
